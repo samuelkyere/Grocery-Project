25fae7f229fe1f1158db6f5f21664e0caf3233dd
Samuel Kyere <samuelboatengkyere@yahoo.com>
2022-01-14 16:55:14 -0500

    Groceries Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import {Component} from 'react'
+import GroceryList from './components/GroceryList'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            item: 'Mango',
+            units: '3lbs',
+            quantity: 1,
+            isPurchased: false
+        }
+    }
+
+    handleChange = event => {// handle change to care of the changes that may occur
+            this.setState({[event.target.id]: event.target.value})
+            console.log(this.state)
+    }
+   
+        handleSubmit = event => {
+            event.preventDefault();
+            const newItem = App.parseForm(event.target);
+            const item = this.state.item.concat(newItem);
+
+            this.setState({item});
+            App.resetForm(event.target);
+        
+        };
+          render() {
+            return(
+        <div>
+          <label htmlFor="item"> Item: </label>
+          <input
+                id="item"
+                onChange={this.handleChange}
+                value={this.state.item} />
+                <label htmlFor="units"> Unit : </label>
+                <input id="units" onChange={this.handleChange}
+                value={this.state.units}
+                type="units"/>
+                <label htmlFor="quantity"> Quantity : </label>
+                <input id="quantity" onChange={this.handleChange}
+                value={this.state.quantity}
+                type="quantity"/>
+                <button>Submit</button>
+                <GroceryList/>
+                <div>
+        <div>{this.state.item}</div> : <div>{this.state.units}</div> - <div>{this.state.quantity}</div> 
+                </div>
+          </div>
+
+ 
+            )
+
+          }
+      }
+
+    
 
 export default App;
---
+++ src/components/GroceryList.js
@@ -0,0 +1,46 @@
+import  {Component} from 'react';
+class GroceryList extends Component {
+    render() { 
+        return(null);
+       // (const items= this.props.items.map((item,index) => <Item item={item} key={index} />);
+        // const notPurchased = <span> Items not purchased</span>;
+
+
+        // return(
+        //     <div className="box">
+        //         {}
+
+        //     </div>
+        
+        }
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         item: '',
+    //         units: '',
+    //         quantity: 0,
+    //         isPurchased: false
+    //     }
+    // }
+
+    // handleChange = event => {// handle change to care of the changes that may occur
+    //         this.setState({[event.target.id]: event.target.value})
+    // }
+    // handleSubmit(event) {
+    //     handleSubmit = event => {
+    //         event.preventDefault();
+    //         const newItem = App.parseForm(event.target);
+    //         const item = this.state.item.concat(newItem);
+
+    //         this.setState({item});
+    //         App.resetForm(event.target);
+    //     }
+    //     };
+
+
+//         }
+//     }
+
+}
+
+export default GroceryList;
